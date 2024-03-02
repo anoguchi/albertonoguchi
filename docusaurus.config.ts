932708a7ffebc2d8b20f5b1cabb04910c57af34a
Alberto Noguchi <alberto2611@gmail.com>
2024-03-02 20:42:51 -0300

    playing with creating pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Index (Zet)',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
@@ -85,7 +85,7 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
+              label: 'Index (Zet)',
               to: '/docs/intro',
             },
           ],
