bd2eca0c80f74fc1ce2f2f4e9499a60295e8b1cf
Alberto Noguchi <alberto2611@gmail.com>
2024-03-19 22:52:52 -0300

    add: search bar v3

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -34,6 +34,7 @@ const config: Config = {
         // ... Your options.
         // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
+        indexPages: true,
         // For Docs using Chinese, The `language` is recommended to set to:
         // ```
         // language: ["en", "zh"],
